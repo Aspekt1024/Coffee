c1a1c16aac0b5183fdbb4dc2aeda36877bb7a41b
Dan <d.szabo87@gmail.com>
2020-03-05 00:48:32 +1100

    Wall generation
    Wall builder added to the House Editor (Window > House Editor). Currently prototype stage, requires logic for joining walls with corners and T intersections.

--- Assets/Scripts/Data/TagDefnitions.cs
+++ Assets/Scripts/Data/TagDefnitions.cs
@@ -3,5 +3,6 @@ namespace Coffee
     public static class TagDefnitions
     {
         public const string Player = "Player";
+        public const string GeneratorParent = "GeneratorParent";
     }
 }
